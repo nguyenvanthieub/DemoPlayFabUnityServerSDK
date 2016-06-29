5a63e74832513958547551ec027f7af194a870eb
Thieu Mao <thieumao@gmail.com>
2016-06-30 02:04:57 +0700

    Demo PlayFab Unity Server

---
+++ Assets/Scripts/PlayFab/PlayFabManager.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+using PlayFab;
+using PlayFab.ServerModels;
+using Assets.Scripts.Boss;
+
+public class PlayFabManager : MonoBehaviour {
+
+    #region PlayFab
+    [ContextMenu("Send All Data To PlayFab")]
+    public void SendAllDataToPlayFab()
+    {
+        SendDataFromJson();
+        SendLevels();
+        SendLevelsTower();
+        SendDataGame();
+    }
+
+    private void SetTitleData(string key, string value)
+    {
+        var updateRequest = new PlayFab.ServerModels.SetTitleDataRequest();
+        updateRequest.Key = key;
+        updateRequest.Value = value;
+        PlayFabServerAPI.SetTitleData(updateRequest, (result) =>
+        {
+            Debug.Log("Set titleData successful");
+        },
+        (error) =>
+        {
+            Debug.Log("Got error setting titleData:");
+            Debug.Log(error.ErrorMessage);
+        });
+    }
+    #endregion
+
+    #region Send Data From Json
+    [ContextMenu("Send Data From Json")]
+    public void SendDataFromJson()
+    {
+        string urlInfo = "http://nhadandungaz.com/petvengers/v1.1.1/info.php";
+        ConnectionManager.Instance.SynsDataFromServer(urlInfo, GetInfoCallback);
+    }
+
+    private void GetInfoCallback(string message) {
+        if (message == null || message == "" || message.Length < 10)
+        {
+            print("Error");
+        } else
+        {
+            var jsonAll = Json.Deserialize(message) as Dictionary<string, object>;
+
+            var jsonInfo = jsonAll["info"] as List<object>;
+            if (jsonInfo != null)
+            {
+                int i = 0;
+                foreach (var item in jsonInfo)
+                {
+                    var dict = item as Dictionary<string, object>;
+                    string name = dict["name"] + "";
+                    string data = dict["data"] + "";
+                    print(++i + "info: " + name + " " + data);
+                    SetTitleData(name, data);
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region Levels.xml
+    [ContextMenu("Send Levels.xml")]
+    public void SendLevels()
+    {
+        var xmlFileMap = Resources.Load("Maps/Levels", typeof(TextAsset)) as TextAsset;
+        var xmlContent = xmlFileMap.text;
+        print("Levels.xml: " + xmlContent);
+        SetTitleData("Levels.xml", xmlContent);
+    }
+    #endregion
+
+    #region LevelsTower.xml
+    [ContextMenu("Send LevelsTower.xml")]
+    public void SendLevelsTower()
+    {
+        var xmlFileMap = Resources.Load("Maps/LevelsTower", typeof(TextAsset)) as TextAsset;
+        var xmlContent = xmlFileMap.text;
+        print("LevelsTower.xml: " + xmlContent);
+        SetTitleData("LevelsTower.xml", xmlContent);
+    }
+    #endregion
+
+    #region DataGame
+    [ContextMenu("Send DataGame")]
+    public void SendDataGame()
+    {
+        SetTitleData("TableBoss",DataGame.Instance.GetTableBoss());
+        SetTitleData("TableBossTower", DataGame.Instance.GetTableBossTower());
+        SetTitleData("TableDraw", DataGame.Instance.GetTableDraw());
+        SetTitleData("TableEnergy", DataGame.Instance.GetTableEnergy());
+        SetTitleData("TableHero", DataGame.Instance.GetTableHero());
+        SetTitleData("TableHeroCastle", DataGame.Instance.GetTableHeroCastle());
+        SetTitleData("TableLevel", DataGame.Instance.GetTableLevel());
+        SetTitleData("TableLevelTower", DataGame.Instance.GetTableLevelTower());
+        SetTitleData("TableReward", DataGame.Instance.GetTableReward());
+        SetTitleData("TableSkill", DataGame.Instance.GetTableSkill());
+        SetTitleData("TableUnlockHero", DataGame.Instance.GetTableUnlockHero());
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PlayFab/UpData.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ServerModels;
+
+public class UpData : MonoBehaviour
+{
+    public string UserB = "45EB0EDEFDA5BEFB";
+
+    [ContextMenu("Up Data UserB")]
+    public void UpdateUserData()
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data = ReadFile();
+        UpdateUserDataRequest request = new UpdateUserDataRequest()
+        {
+            PlayFabId = UserB,
+            Data = data
+        };
+
+        PlayFabServerAPI.UpdateUserData(
+            request,
+            (result) =>
+            {
+                Debug.Log("UpdateUserData successful");
+            },
+            (error) =>
+            {
+                Debug.Log("UpdateUserData error:");
+                Debug.Log(error.ErrorMessage);
+            }
+        );
+    }
+
+    private Dictionary<string, string> ReadFile()
+    {
+        string link = Application.persistentDataPath + "/DataUser.txt";
+        print("link= " + link);
+        StreamReader sr = new StreamReader(link);
+        string line1 = "";
+        string line2 = "";
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        while (line1 != "|||")
+        {
+            line1 = sr.ReadLine();
+            line2 = sr.ReadLine();
+            if (line1 != "|||")
+                data.Add(line1, line2);
+        }
+        sr.Close();
+        return data;
+    }
+}
